8f037ae3b9f3198e97886fae3e16b146202d56fc
Javohir <test2023@gmail.com>
2023-12-05 18:24:36 +0500

    save

--- src/store/index.js
+++ src/store/index.js
@@ -20,7 +20,7 @@ export default createStore({
         let {lat, lon, local_names} = response.data[0]
         if(local_names.ru.toLowerCase() == city.toLowerCase()){
           let response2 = await axios.get(
-          `https://api.openweathermap.org/data/2.8/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${context.state.apiKey}&units=metric&lang=ru`)
+          `http://api.openweathermap.org/data/2.8/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${context.state.apiKey}&units=metric&lang=ru`)
           let watherInfo = {...response2, name: city}
           context.commit('getWeather', watherInfo)
         }
